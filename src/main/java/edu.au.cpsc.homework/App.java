c137b88886b1001700c40252b3d171a4bc400254
AppsByJoe <jth0124@auburn.edu>
2022-10-06 10:27:48 -0400

    Project initialized, maven build complete.

---
+++ src/main/java/edu.au.cpsc.homework/App.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2022. AppsByJoe. See LICENSE.txt for complete copyright information.
+ */
+
+package edu.au.cpsc.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main driver class.
+ */
+@SpringBootApplication
+public class App {
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+}
+
